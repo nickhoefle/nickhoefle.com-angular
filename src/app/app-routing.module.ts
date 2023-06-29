4e305114bc9a26d660670a2961445984c38a26a1
Nick <nickhoefle@gmail.com>
2023-06-29 17:44:53 -0500

    Added homepage content/component. Fixed routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageContentComponent } from './homepage-content/homepage-content.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
+  { path: '', component: HomepageContentComponent },
   { path: 'projects', component: ProjectsComponent }
 ];
 
